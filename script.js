4d67caa36e0b276eee3a43981577610fc874c4d1
Gabriel Lovato Vianna <gabriellovato15@gmail.com>
2020-03-23 22:27:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function censors( texto ){
+    let censor = texto.toLowerCase() || prompt("Digite o texto que você quer censurar.");
+
+    let alpha = "abcdefghijklmnopqrstuvwxyzç";
+
+    for(var i = 0; i < censor.length; i++){
+        if(alpha.includes(censor[i]) == true){
+            censor = censor.replace(censor[i], "*");
+        }
+    }
+    document.getElementById("result").innerHTML = censor;
+}
